11e9f6f2f20a56170451ac44288f0c5b4570999e
Simon Vig Therkildsen <simonvt@gmail.com>
2012-08-16 16:59:32 +0200

    Add sample.

---
+++ samples/src/net/simonvt/calendarview/samples/CalendarSamples.java
@@ -0,0 +1,14 @@
+package net.simonvt.calendarview.samples;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalendarSamples extends Activity {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
